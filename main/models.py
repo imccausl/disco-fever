0850ac9166fd15df51c978ad9501e48eaee231ad
Ian McCausland <ian.mccausland@tophat.com>
2022-04-28 13:23:10 -0400

    create golf course model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class GolfCourse(models.Model):
+    name = models.CharField(max_length=1000)
+    lat = models.DecimalField(max_digits=10, decimal_places=8)
+    lng = models.DecimalField(max_digits=11, decimal_places=8)
