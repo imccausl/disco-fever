98a9ca674071960fe433de8066bb5da8f1af94c6
Mark Clapa <mark.clapa@tophatmonocle.com>
2022-10-06 12:29:10 -0400

    wip: next js files have /static in front

--- disco_fever/settings.py
+++ disco_fever/settings.py
@@ -124,10 +124,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "_next/static/"
+STATIC_URL = "static/"
 
 STATICFILES_DIRS = [
-    BASE_DIR / "my-app" / "out" / "_next" / "static",
+    BASE_DIR / "my-app" / "out",
 ]
 
 # Default primary key field type
--- my-app/next.config.js
+++ my-app/next.config.js
@@ -1,7 +1,10 @@
+const buildForDjango = process.argv.includes('build')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   compress: false,
+  assetPrefix: buildForDjango ? '/static' : undefined,
 }
 
 module.exports = nextConfig
