d48f9514f7988acdc4b8055e8c0d8fdc60ebcb8c
Mark Clapa <mark.clapa@tophatmonocle.com>
2022-10-06 13:02:42 -0400

    wip: collectstatic configured for nginx to dump into staticfiles

--- disco_fever/settings.py
+++ disco_fever/settings.py
@@ -130,6 +130,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "my-app" / "out",
 ]
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
