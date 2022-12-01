04f280b5149944e79f93371d1cb201643976ebb9
Ian McCausland <ian.mccausland@tophat.com>
2022-12-01 13:18:57 -0500

    wip: fix deploy script

--- config/scripts/deploy.sh
+++ config/scripts/deploy.sh
@@ -4,8 +4,10 @@ CONTAINER_ID="$(docker container ls  | grep 'disco_fever:v0.1' | awk '{print $1}
 
 docker build . -f config/django/Dockerfile -t disco_fever:v0.1
 
-if [ -z "$CONTAINER_ID" ]; then
-    docker stop $CONTAINER_ID
+
+if [ -n "$CONTAINER_ID" ]; then
+    echo "container id found, stopping: $CONTAINER_ID"
+    docker stop "$CONTAINER_ID"
 fi
 
 docker run -d -p 8000:8000 disco_fever:v0.1
