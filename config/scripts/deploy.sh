912d8666568ccd1abfa323cd14920299783d0ebb
Ian McCausland <ian.mccausland@tophat.com>
2022-12-01 13:34:31 -0500

    wip: stop any containers on port 8000

--- config/scripts/deploy.sh
+++ config/scripts/deploy.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-CONTAINER_ID="$(docker container ls  | grep 'disco_fever:v0.1' | awk '{print $1}')"
+CONTAINER_ID="$(docker container ls  | grep '0.0.0.0:8000' | awk '{print $1}')"
 
 docker build . -f config/django/Dockerfile -t disco_fever:v0.1
 
