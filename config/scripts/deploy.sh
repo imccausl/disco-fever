fd40f14285fac02ef5786db32609537b03097a3b
Ian McCausland <ian.mccausland@tophat.com>
2022-12-15 13:34:16 -0500

    wip: checkout branch in deploy step

--- config/scripts/deploy.sh
+++ config/scripts/deploy.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
-CONTAINER_ID="$(docker container ls  | grep '0.0.0.0:8000' | awk '{print $1}')"
-
-docker-compose -f config/docker-compose.yml build -d
+docker-compose -f config/docker-compose.yml build
 docker-compose -f config/docker-compose.yml down
 docker-compose -f config/docker-compose.yml up -d
