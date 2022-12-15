c08fe2036bb9b3e8454a925436d363f801f6d450
Ian McCausland <ian.mccausland@tophat.com>
2022-12-15 13:27:14 -0500

    wip: docker-compose the things

--- config/scripts/deploy.sh
+++ config/scripts/deploy.sh
@@ -2,12 +2,6 @@
 
 CONTAINER_ID="$(docker container ls  | grep '0.0.0.0:8000' | awk '{print $1}')"
 
-docker build . -f config/django/Dockerfile -t disco_fever:v0.1
-
-
-if [ -n "$CONTAINER_ID" ]; then
-    echo "container id found, stopping: $CONTAINER_ID"
-    docker stop "$CONTAINER_ID"
-fi
-
-docker run -d -p 8000:8000 disco_fever:v0.1
+docker-compose -f config/docker-compose.yml build -d
+docker-compose -f config/docker-compose.yml down
+docker-compose -f config/docker-compose.yml up -d
