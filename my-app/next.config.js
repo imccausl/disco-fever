9151039d73d20b228343d615cc8d20c5f367a51c
Mark Clapa <mark.clapa@tophatmonocle.com>
2023-02-08 10:58:56 -0500

    feat: nginx now routes to nextjs serve on 404
    summary of changes
    > nginx file update
    > use babel for compilation instead of rust-based swcMinify,
    	because nextjs errors out when returning response otherwise
    	due to hot reload
    > commented out login urls in Django so we can fallback
    > NEW docker nextjs image: config, and docker-compose setup

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -5,6 +5,13 @@ const nextConfig = {
   reactStrictMode: true,
   compress: false,
   assetPrefix: buildForDjango ? '/static' : undefined,
+  webpackDevMiddleware: config => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
